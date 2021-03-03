c71f90ef54cdf088208c4b144cc3852ba801e314
ryota <ryota@ikumiryoutanoMacBook-Pro.local>
2021-03-03 20:52:33 +0900

    first commit

---
+++ pages/about.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="container">
+    <nuxt-link to="/">TOP</nuxt-link>
+    <div>
+      <h1>ABOUT</h1>
+      <h2>About Me</h2>
+      <li>name:ryota ikumi</li>
+      <li>age:32</li>
+      <li>country:Japan</li>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+</style>
